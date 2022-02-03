df39e6e859124b2ddae5599cf41a39281a8ce821
meepsauce <gillespietheo@gmail.com>
2022-02-03 18:55:37 +0000

    redid entire pipeline...

--- BALLS/graphics/graphics.h
+++ BALLS/graphics/graphics.h
@@ -1,19 +1,86 @@
 #pragma once
 
+#include <cstddef>
 #define FPS 30
 #define WIDTH 800   
 #define HEIGHT 450
 
-#include "raylib.h"
+#include "../core/core.h"
+#include "../io/io.h"
 
-class Graphics {
+#include <GLFW/glfw3.h>
+
+#ifdef _WIN32
+
+#else
+#include "../draw/debug_draw.hpp"
+
+#endif
+
+#define DEBUG_DRAW_IMPLEMENTATION
+using namespace dd;
+
+
+class GL {
     public:
-        Camera3D camera = {0};
+        GLFWwindow* window;
 
-        Graphics() {};
+        GL() {};
 
         void init() {
-            InitWindow(WIDTH, HEIGHT, "BALListic Simulator");
-            camera.position = (Vector3){0,0,0}
+            if (!glfwInit()) {
+                Console::error("Failed to initalize graphics (fatal)");
+                exit(3);
+            }
+            this->window = glfwCreateWindow(WIDTH, HEIGHT, "BALListic Simulator", NULL, NULL);
+            if (!window)
+            {
+                glfwTerminate();
+                Console::error("Failed to initalize window (fatal)");
+                exit(3);
+            }
+            glfwMakeContextCurrent(window);
+            Console::print("Done!");
+
+            glfwTerminate();
+        };
+
+        //draws a singular frame
+        void draw() {
+
+            glClear(GL_COLOR_BUFFER_BIT);
+            /* Swap front and back buffers */
+            glfwSwapBuffers(window);
+            /* Poll for and process events */
+            glfwPollEvents();
+        };
+
+        void end() {
+            glfwTerminate();
+        };
+};
+
+class DebugRenderInterface : RenderInterface {
+public:
+    GL* graphics;
+    DebugRenderInterface() {
+        this->graphics = new GL();
+    }
+
+    void beginDraw() override {
+        this->graphics->init();
+    };
+
+    void endDraw() override {
+        this->graphics->end();
+    };
+
+    void drawPointList(const DrawVertex * points, int count, bool depthEnabled) override {
+        if(points == nullptr && count <= 0 && count > DEBUG_DRAW_VERTEX_BUFFER_SIZE) {
+            return;
         }
-}
+        
+
+    };
+    void drawLineList(const DrawVertex * lines, int count, bool depthEnabled) override;
+};
--- setup.sh
+++ setup.sh
@@ -11,18 +11,13 @@ sudo apt-get -y install cmake
 sudo apt-get -y install git
 
 echo ------------------------------------
-echo Installing Required Graphics and Audio libraries...
-sudo apt install libasound2-dev mesa-common-dev libx11-dev libxrandr-dev libxi-dev xorg-dev libgl1-mesa-dev libglu1-mesa-dev
+echo Installing GLFW
+sudo apt-get install libglfw3
+sudo apt-get install libglfw3-dev
 
 echo ------------------------------------
-echo Installing RayLib....
-git clone https://github.com/raysan5/raylib.git raylib
-cd raylib/src/
-
-echo ------------------------------------
-echo Building RayLib...
-make PLATFORM=PLATFORM_DESKTOP
-sudo make install
+echo Installing Debug-Draw...
+git clone https://github.com/glampert/debug-draw.git BALLS/draw
 
 echo ------------------------------------
 echo Successfully installed and built dependecies
